f3b16ba9eca8eb8ef25fc97996d9c38f48a6008c
elgar23 <superel2303@gmail.com>
2021-02-26 16:12:34 +0200

    hw13

---
+++ src/js/apiService.js
@@ -0,0 +1,36 @@
+const BASE_URL = 'https://pixabay.com/api';
+const API_KEY = '20398947-d1e437c4ec35c84ea5c56c206';
+
+export default class ImageApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async fetchImage() {
+    const responseUrl = await fetch(
+      `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`,
+    );
+
+    const { hits: images } = await responseUrl.json();
+    this.incrementPage();
+
+    return images;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
